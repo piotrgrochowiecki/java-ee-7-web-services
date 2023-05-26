44d0a83b192fae860f00f133308e006e1c4565c1
pgrochowieck <piotr.grochowiecki@dxc.com>
2023-05-26 20:25:47 +0200

    Building a REST client: get request for list of objects

--- restclient/src/main/java/com/keysoft/RestClient.java
+++ restclient/src/main/java/com/keysoft/RestClient.java
@@ -4,7 +4,9 @@ package com.keysoft;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class RestClient {
@@ -25,13 +27,21 @@ public class RestClient {
 //        System.out.println("Returned application: " + application.toString());
 
         //Create a single application
-        client.target("http://localhost:8080/javaee-7.0/rest/applications")
-                .request()
-                .post(Entity.entity(new Application(59, "Channel List", "TV guide app"),
-                                    MediaType.APPLICATION_JSON_TYPE),
-                      Application.class);
+//        client.target("http://localhost:8080/javaee-7.0/rest/applications")
+//                .request()
+//                .post(Entity.entity(new Application(59, "Channel List", "TV guide app"),
+//                                    MediaType.APPLICATION_JSON_TYPE),
+//                      Application.class);
+//
+//        logger.info("Application created");
 
-        logger.info("Application created");
+        //Return a list of applications
+        List<Application> applications = client.target("http://localhost:8080/javaee-7.0/rest")
+                .path("applications")
+                .request(MediaType.APPLICATION_JSON)
+                .get(new GenericType<List<Application>>() {});
+
+        logger.info("Returned applications: " + applications.toString());
     }
 
 }
