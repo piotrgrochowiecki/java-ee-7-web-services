7a86ead7afc804da739f95911c9f1a90650a74ca
pgrochowieck <piotr.grochowiecki@dxc.com>
2023-05-26 20:16:02 +0200

    Building a REST client: post request

--- restclient/src/main/java/com/keysoft/RestClient.java
+++ restclient/src/main/java/com/keysoft/RestClient.java
@@ -3,20 +3,35 @@ package com.keysoft;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import java.util.logging.Logger;
 
 public class RestClient {
 
     public static void main(String[] args) {
+        Logger logger = Logger.getAnonymousLogger();
+
         Client client = ClientBuilder.newClient();
 
-        Application application = client
-                .target("http://localhost:8080/javaee-7.0/rest/applications")
-                .path("{id}")
-                .resolveTemplate("id", 1)
-                .request()
-                .get(Application.class);
+        //Get single application
+//        Application application = client
+//                .target("http://localhost:8080/javaee-7.0/rest/applications")
+//                .path("{id}")
+//                .resolveTemplate("id", 1)
+//                .request()
+//                .get(Application.class);
+//
+//        System.out.println("Returned application: " + application.toString());
 
-        System.out.println("Returned application: " + application.toString());
+        //Create a single application
+        client.target("http://localhost:8080/javaee-7.0/rest/applications")
+                .request()
+                .post(Entity.entity(new Application(59, "Channel List", "TV guide app"),
+                                    MediaType.APPLICATION_JSON_TYPE),
+                      Application.class);
+
+        logger.info("Application created");
     }
 
 }
