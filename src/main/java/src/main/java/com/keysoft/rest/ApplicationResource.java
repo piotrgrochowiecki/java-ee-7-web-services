b8a3ee3e034f9e3585671af5f9abd0fcd3bcd8dc
pgrochowieck <piotr.grochowiecki@dxc.com>
2023-05-26 17:22:52 +0200

    get all applications from database

--- src/main/java/src/main/java/com/keysoft/rest/ApplicationResource.java
+++ src/main/java/src/main/java/com/keysoft/rest/ApplicationResource.java
@@ -17,20 +17,43 @@ import java.util.List;
 public class ApplicationResource {
 
     @GET
-    public List<Application> getAll() { //http://localhost:8080/javaee-7.0/rest/applications
-        List<Application> testList = new ArrayList<>();
+    public Response getAll() { //http://localhost:8080/javaee-7.0/rest/applications
+        List<Application> applicationList = new ArrayList<>();
 
-        testList.add(new Application(1, "name 1", "desc 1"));
-        testList.add(new Application(2, "name 2", "desc 2"));
-        testList.add(new Application(3, "name 3", "desc 3"));
+        try {
+            Statement statement = Database.getConnection()
+                    .createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM tza_application");
 
-        return testList;
+            while(resultSet.next()) {
+                applicationList.add(new Application(resultSet.getInt(1),
+                                                    resultSet.getString(2),
+                                                    resultSet.getString(3)));
+            }
+        } catch(Exception exception) {
+            exception.printStackTrace();
+        }
+
+        return Response.status(200).entity(applicationList).build();
 
     }
 
     @POST
     public Application addApplication(Application application) {
         return application;
+    }
+
+    @GET
+    @Path("{id}")
+    public Application getApplication(@PathParam("id") int id) {
+        return new Application(id, "name " + id, "desc " + id);
+    }
+
+    @GET
+    @Path("{id}/{name}")
+    public Application getApplication(@PathParam("id") int id,
+                                      @PathParam("name") String name) {
+        return new Application(id, name + " " + id, "desc " + id);
     }
 
 }
